7e3a50e93b8bc01276669a3f88f4b1cb0ba73d2f
AhmedMadian <ahmed.mostafa.madian@gmail.com>
2020-09-23 20:44:25 +0200

    Project intial Setup

---
+++ MarleySpoon/MarleySpoon/Core/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  MarleySpoon
+//
+//  Created by Ahmed Madian on 9/23/20.
+//  Copyright © 2020 Ahmed Madian. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+}
+
---
+++ MarleySpoon/MarleySpoon/Core/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  MarleySpoon
+//
+//  Created by Ahmed Madian on 9/23/20.
+//  Copyright © 2020 Ahmed Madian. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+    
+}
+
